e1636e0a483ea9cc194392bffff8d22aeb00b54f
Marcos Ojeda <subliminal@gmail.com>
2018-04-28 14:00:58 -0700

    move babel config into @nsfmc/babel-preset-sketching

--- utils/babel-preset-sketching/index.js
+++ utils/babel-preset-sketching/index.js
@@ -1,22 +1,24 @@
-module.exports = {
-  presets: [
-    [
-      require("@babel/preset-env"),
-      {
-        targets: {
-          browsers: [">0.25%", "not ie 11", "not op_mini all"]
+module.exports = function(api, opts) {
+  return {
+    presets: [
+      [
+        require("@babel/preset-env"),
+        {
+          targets: {
+            browsers: [">0.25%", "not ie 11", "not op_mini all"]
+          }
         }
-      }
+      ],
+      require("@babel/preset-flow"),
+      require("@babel/preset-react")
     ],
-    require("@babel/preset-flow"),
-    require("@babel/preset-react")
-  ],
-  plugins: [
-    require("@babel/plugin-proposal-class-properties"),
-    require("@babel/plugin-proposal-object-rest-spread"),
-    require("@babel/plugin-transform-classes"),
-    require("@babel/plugin-transform-destructuring"),
-    require("@babel/plugin-transform-react-constant-elements"),
-    require("@babel/plugin-transform-react-display-name")
-  ]
+    plugins: [
+      require("@babel/plugin-proposal-class-properties").default,
+      require("@babel/plugin-proposal-object-rest-spread").default,
+      require("@babel/plugin-transform-classes").default,
+      require("@babel/plugin-transform-destructuring").default,
+      require("@babel/plugin-transform-react-constant-elements").default,
+      require("@babel/plugin-transform-react-display-name").default
+    ]
+  };
 };
