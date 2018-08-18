65632857572a582c59aa24b4e9510e4c49b903e7
Marcos Ojeda <subliminal@gmail.com>
2018-08-17 20:16:27 -0700

    simplify and use sketching

--- electrocat/.babelrc.js
+++ electrocat/.babelrc.js
@@ -1,16 +1,5 @@
 module.exports = {
   presets: [
-    '@babel/preset-react',
-    '@babel/preset-flow',
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          browsers: ['Electron >= 1.7.11'],
-        },
-        useBuiltIns: 'entry',
-      },
-    ],
-  ],
-  plugins: [['@babel/plugin-proposal-class-properties', {loose: true}]],
+    '@nsfmc/babel-preset-sketching'
+  ]
 };
